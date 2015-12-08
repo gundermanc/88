4d4e5d44341089549c3dfffd1de7cef893080228
Christian Gunderman <gundermanc@gmail.com>
2015-12-08 18:45:06 -0500

    88 Exception base class.

---
+++ Common88/Common88/Exception88.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="Exception88.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author>
+//-----------------------------------------------------------------------
+
+namespace Uniscript
+{
+    using System;
+
+    /// <summary>
+    /// The parent class for all publicly emitted exceptions that occur under
+    /// normal operation.
+    /// </summary>
+    public abstract class Exception88 : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Exception88"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="inner">The inner exception that caused this exception.</param>
+        protected Exception88(string message = null, Exception inner = null) : base(message, inner)
+        {
+        }
+    }
+}
